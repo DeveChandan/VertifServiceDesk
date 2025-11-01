868eb9ff9f151ddfaed90ff6f966ef94c441686e
DeveChandan <cm6322319@gmail.com>
2025-11-01 12:11:07 +0530

    config11

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import appPromise from '../dist/index.js';
+import appPromise from '../server/index.ts';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const app = await appPromise;
